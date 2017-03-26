62bcb7ca1b6a57e33e4ab32f0da67c31775b804c
Vinicius Min Soo <Vinicius Min Soo>
2017-03-26 13:19:40 -0300

    'win'

--- content/Documento sem título.md
+++
@@ -1,7 +0,0 @@
-Title: Hello
-Date: 2017-03-26 12:04
-Category: social
-Tags: intro
-Slug: First-post
-Authors: Vinicius Min Soo
-Summary: Ola
--- pelicanconf.py
+++ pelicanconf.py
@@ -20,10 +20,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
+"""LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('You can modify those links in your config file', '#'),)"""
 
 # Social widget
 SOCIAL = (('linkedin', 'https://www.linkedin.com/in/vinicius-min-soo-81b41211b/'),
